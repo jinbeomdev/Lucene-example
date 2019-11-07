636788ce17ce59a53a760c252e1e10082e83103d
jinbeomdev <niutn@naver.com>
2019-11-07 12:40:16 +0900

    init

---
+++ src/test/java/com/github/jinbeomdev/Luceneexample/LuceneExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jinbeomdev.Luceneexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuceneExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
